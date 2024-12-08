273c51f7a9ebaa440fc292fe9c1319382b2b5d1f
jsrafi <js.rafi999@gmail.com>
2024-12-08 20:29:45 +0600

    hulf done

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
+
+// adding & removing classlist ('hidden)
 document.getElementById('history-btn').addEventListener('click', function()
 {
+    document.getElementById('history-btn').classList.add('bg-[#B4F461]')
+    document.getElementById('donation-btn').classList.remove('bg-[#B4F461]')
     addClass('donation-section');
     removeClass('history-section');
     
 })
 document.getElementById('donation-btn').addEventListener('click', function()
 {
+    document.getElementById('history-btn').classList.remove('bg-[#B4F461]')
+    document.getElementById('donation-btn').classList.add('bg-[#B4F461]')
     removeClass('donation-section');
     addClass('history-section');
     
 })
+
+// blog page to home page
+document.getElementById('blog-btn').addEventListener('click',function()
+{
+    window.location.href = '/blog.html'
+})
+
+
